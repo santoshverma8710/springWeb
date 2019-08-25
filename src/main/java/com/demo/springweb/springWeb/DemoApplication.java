dd2116ad7577a0c89adea1c9373fb97c87299395
santoshverma8710 <50474494+santoshverma8710@users.noreply.github.com>
2019-08-25 13:33:02 +0530

    added import for request mapping

--- src/main/java/com/demo/springweb/springWeb/DemoApplication.java
+++ src/main/java/com/demo/springweb/springWeb/DemoApplication.java
@@ -3,6 +3,7 @@ package com.demo.springweb.springWeb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @SpringBootApplication
