e208176bcf6b8a9f96d621fd50914d95bf2feb66
santoshverma8710 <50474494+santoshverma8710@users.noreply.github.com>
2019-08-25 13:24:39 +0530

    added rest controller

--- src/main/java/com/demo/springweb/springWeb/DemoApplication.java
+++ src/main/java/com/demo/springweb/springWeb/DemoApplication.java
@@ -2,12 +2,20 @@ package com.demo.springweb.springWeb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@RequestMapping("/hello")
+	public string hello()
+	{
+		return "Hello World";
 	}
 
 }
